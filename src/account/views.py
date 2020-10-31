a59299e27e71785bb18f2a473712c313bcdd63ed
adityamulik <aditya.mulik@gmail.com>
2020-10-31 04:29:11 -0400

    Added authentication using class-based views

--- src/account/views.py
+++ src/account/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .forms import LoginForm
 
-# Create your views here.
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
